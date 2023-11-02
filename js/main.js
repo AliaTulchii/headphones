85d372c5213ced01791e02164f17a32a8eb20646
Halina Tulchii <halendeihalyna@gmail.com>
2023-11-02 15:31:35 +0200

    added blur header

--- js/main.js
+++ js/main.js
@@ -27,6 +27,15 @@ const linkAction = () => {
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 // add blur header
+
+const blurHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+        : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll', blurHeader)
 // swiper favourites
 // show scroll up
 // scroll sections active link
